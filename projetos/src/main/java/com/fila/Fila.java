a2a79509db449b9ce4f80e034dcdbb621a0cf02f
camila monteiro <monteirocamis@gmail.com>
2022-02-27 23:18:46 -0300

    :art: A1 and files from dio course

--- projetos/src/main/java/com/fila/Fila.java
+++ projetos/src/main/java/com/fila/Fila.java
@@ -1,4 +1,110 @@
 package com.fila;
 
-public class Fila {
+public class Fila<T>  {
+
+    private No<T> refNoEntradaFila;
+
+    public Fila() {
+        this.refNoEntradaFila = null;
+
+    }
+
+
+
+
+
+    public void enqueue(T object){
+        No novoNo = new No(object);
+
+        novoNo.setRefNo(refNoEntradaFila);       //--->           entra na fila aponta pro ultimo e muda pro prox
+        refNoEntradaFila = novoNo;               //--->          muda ref entrada o novono
+
+    }
+
+
+
+
+
+    //quem eh o primeiro da fila
+    public T first(){
+        if(!this.isEmpty()){
+            No primeiroNo = refNoEntradaFila;
+            while(true){
+                if(primeiroNo.getRefNo() != null){ // se nao tiver nos a frente
+                    primeiroNo = primeiroNo.getRefNo(); //pega e passa a ser o prox da fila
+                }else{
+                    break;
+                }
+            }
+            return (T) primeiroNo.getObject();
+        }
+        return null;
+    }
+
+
+
+
+
+//desenfilerar
+    public T dequeue(){
+        if(!this.isEmpty()){
+            No primeiroNo = refNoEntradaFila;
+            No noAuxiliar = refNoEntradaFila;
+            while(true){
+                if(primeiroNo.getRefNo() != null){ // se nao tiver nos a frente
+                    noAuxiliar = primeiroNo;
+                    primeiroNo = primeiroNo.getRefNo(); //pega e passa a ser o prox da fila
+
+                }else{
+                    noAuxiliar.setRefNo(null);
+                    break;
+                }
+            }
+            return (T) primeiroNo.getObject();
+        }
+        return null;
+    }
+
+
+
+
+
+    public  boolean isEmpty(){
+        return refNoEntradaFila == null ? true : false;
+    }
+
+
+
+
+
+    @Override
+    public String toString() {
+        String stringRetorno = "";
+        No noAuxiliar = refNoEntradaFila;
+
+        if(refNoEntradaFila != null){   //se a fila nao estivet vazia
+            while(true){
+                stringRetorno += "[No { objeto= " + noAuxiliar.getObject() + " }] --> ";
+
+                if(noAuxiliar.getRefNo() != null){
+                    noAuxiliar = noAuxiliar.getRefNo();
+                }else{
+                    stringRetorno += "null";
+                    break;
+                }
+            }
+        }else{
+            stringRetorno = "null";
+        }
+
+
+        return stringRetorno;
+    }
 }
+
+
+
+
+
+
+
